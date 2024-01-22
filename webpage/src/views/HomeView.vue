f8e3b32611da7e1a4acb65b1f930bd021510d4de
dpinedaa <86670148+dpinedaa@users.noreply.github.com>
2024-01-22 15:08:15 -0500

    Update HomeView.vue

--- webpage/src/views/HomeView.vue
+++ webpage/src/views/HomeView.vue
@@ -55,7 +55,14 @@
             <!-- Flow row -->
             <tr>
               <td>{{ flow.srcIP }} <i class="fas fa-arrows-alt-h"></i> {{ flow.dstIP }}</td>
-              <td>{{ flow.protocol }}</td>
+              <td>
+                <!-- if protocol is 1 -->
+                <template v-if="flow.fwdFlow.protocol == 1">ICMP</template>
+                <!-- if protocol is 6 -->
+                <template v-else-if="flow.fwdFlow.protocol == 6">TCP</template>
+                <!-- if protocol is 17 -->
+                <template v-else-if="flow.fwdFlow.protocol == 17">UDP</template>
+              </td>
               <td>{{ flow.forwardDuration }}</td>
               <td>{{ flow.forwardPacketCount }}</td>
               <td>{{ flow.forwardByteCount }}</td>
@@ -64,10 +71,66 @@
               <td>{{ flow.backwardByteCount }}</td>
               <td>{{ flow.flowBytesPerSecond }}</td>
               <td>{{ flow.backwardPacketsPerSecond }}</td>
-              <td>{{ flow.lrPrediction }}</td>
-              <td>{{ flow.rfPrediction }}</td>
-              <td>{{ flow.nbPrediction }}</td>
-              <td>{{ flow.knnPrediction }}</td>
+
+
+
+              <td>
+                <!-- Check the vaues for lr  -->
+                <!-- If it's less than 0.5 put the style font color as green and bold  -->
+                <div v-if="flow.lrPrediction < 0.5" style="color: green; font-weight: bold">
+                  {{ flow.lrPrediction }}
+                </div>
+                
+                <!-- Value is between 0.5 and 0.8 font color is orange -->
+                <div v-else-if="flow.lrPrediction >= 0.5 && flow.lrPrediction < 0.8" style="color: orange; font-weight: bold;">
+                  {{ flow.lrPrediction }}
+                </div>
+
+                <!-- Value is between 0.8 and 1 in red-->
+                <div v-else style="color: red; font-weight: bold;">
+                  {{ flow.lrPrediction }}
+                </div>
+              </td>
+
+
+              <td>
+                <div v-if="flow.rfPrediction < 0.5" style="color: green; font-weight: bold">
+                  {{ flow.rfPrediction }}
+                </div>
+                <div v-else-if="flow.rfPrediction >= 0.5 && flow.rfPrediction < 0.8" style="color: orange; font-weight: bold;">
+                  {{ flow.rfPrediction }}
+                </div>
+                <div v-else style="color: red; font-weight: bold;">
+                  {{ flow.rfPrediction }}
+                </div>
+              </td>
+              <td>
+                <div v-if="flow.nbPrediction < 0.5" style="color: green; font-weight: bold">
+                  {{ flow.nbPrediction }}
+                </div>
+                <div v-else-if="flow.nbPrediction >= 0.5 && flow.nbPrediction < 0.8" style="color: orange; font-weight: bold;">
+                  {{ flow.nbPrediction }}
+                </div>
+                <div v-else style="color: red; font-weight: bold;">
+                  {{ flow.nbPrediction }}
+                </div>
+              </td>
+
+              <td>
+                <div v-if="flow.knnPrediction < 0.5" style="color: green; font-weight: bold">
+                  {{ flow.knnPrediction }}
+                </div>
+                <div v-else-if="flow.knnPrediction >= 0.5 && flow.knnPrediction < 0.8" style="color: orange; font-weight: bold;">
+                  {{ flow.knnPrediction }}
+                </div>
+                <div v-else style="color: red; font-weight: bold;">
+                  {{ flow.knnPrediction }}
+                </div>
+
+              </td>
+
+
+              
             </tr>
           </template>
         </tbody>
