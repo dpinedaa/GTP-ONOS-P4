f8f836d25f9c85eeec7cd4b95a9a464eaa9ad42b
dpinedaa <86670148+dpinedaa@users.noreply.github.com>
2024-05-13 13:30:35 -0400

    update

---
+++ BaselineClass.java
@@ -0,0 +1,158 @@
+package us.fiu.adwise.app;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+// import us.fiu.adwise.app.common.Utils;
+import us.fiu.adwise.app.common.FabricDeviceConfig;
+import us.fiu.adwise.app.pipeconf.PipeconfLoader;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.net.URI;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Arrays; // Import the Arrays class
+import java.util.Collection;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.StreamSupport;
+import org.json.JSONObject;
+import org.onlab.packet.Ethernet;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.flow.criteria.Criterion;
+import org.onosproject.net.flow.criteria.IPCriterion;
+import org.onosproject.net.flow.criteria.PiCriterion;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.FlowEntry;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.flow.TableId;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.pi.model.PiActionId;
+import org.onosproject.net.pi.model.PiActionParamId;
+import org.onosproject.net.pi.model.PiMatchFieldId;
+import org.onosproject.net.pi.model.PiPipeconf;
+import org.onosproject.net.pi.runtime.PiAction;
+import org.onosproject.net.pi.runtime.PiActionParam;
+import org.onosproject.net.pi.service.PiPipeconfService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.onosproject.net.config.NetworkConfigRegistry;
+
+// import static us.fiu.adwise.app.AppConstants.PIPECONF_ID;
+// import static us.fiu.adwise.app.AppConstants.APP_NAME;
+// import static us.fiu.adwise.app.AppConstants.CLEAN_UP_DELAY;
+// import static us.fiu.adwise.app.AppConstants.DEFAULT_CLEAN_UP_RETRY_TIMES;
+// import static us.fiu.adwise.app.common.Utils.sleep;
+import static us.fiu.adwise.app.AppConstants.APP_NAME;
+import static us.fiu.adwise.app.AppConstants.CLEAN_UP_DELAY;
+import static us.fiu.adwise.app.AppConstants.DEFAULT_CLEAN_UP_RETRY_TIMES;
+import static us.fiu.adwise.app.common.Utils.sleep;
+import org.onosproject.net.group.GroupService;
+import 
+
+@Component(immediate = true)
+public class BaselineClass {
+    private static final Logger log = LoggerFactory.getLogger(BaselineClass.class.getName());
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    private CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    @SuppressWarnings("unused")
+    protected PipeconfLoader pipeconfLoader;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected NetworkConfigRegistry configRegistry;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    private GroupService groupService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    private DeviceService deviceService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    private FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    private ComponentConfigService compCfgService;
+
+    private final ConfigFactory<DeviceId, FabricDeviceConfig> fabricConfigFactory =
+            new ConfigFactory<DeviceId, FabricDeviceConfig>(
+                    SubjectFactories.DEVICE_SUBJECT_FACTORY, FabricDeviceConfig.class, FabricDeviceConfig.CONFIG_KEY) {
+                @Override
+                public FabricDeviceConfig createConfig() {
+                    return new FabricDeviceConfig();
+                }
+            };
+
+    private ApplicationId appId;
+
+    // For the sake of simplicity and to facilitate reading logs, use a
+    // single-thread executor to serialize all configuration tasks.
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    @Activate
+    protected void activate() {
+        appId = coreService.registerApplication(APP_NAME);
+        
+        // Wait to remove flow and groups from previous executions.
+        waitPreviousCleanup();
+
+        compCfgService.preSetProperty("org.onosproject.net.flow.impl.FlowRuleManager",
+                                      "fallbackFlowPollFrequency", "4", false);
+        compCfgService.preSetProperty("org.onosproject.net.group.impl.GroupManager",
+                                      "fallbackGroupPollFrequency", "3", false);
+        compCfgService.preSetProperty("org.onosproject.provider.host.impl.HostLocationProvider",
+                                      "requestIpv6ND", "true", false);
+        compCfgService.preSetProperty("org.onosproject.provider.lldp.impl.LldpLinkProvider",
+                                      "useBddp", "false", false);
+
+        configRegistry.registerConfigFactory(fabricConfigFactory);
+        log.info("Started");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        configRegistry.unregisterConfigFactory(fabricConfigFactory);
+
+        cleanUp();
+
+        log.info("Stopped");
+    }
+    
+}
